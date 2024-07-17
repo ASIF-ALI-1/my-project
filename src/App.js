6a4c0cc115517143c767724df32e13a10f0c0c75
Asif-Ali-1 <asif7171786@gmail.com>
2024-07-17 20:04:43 +0500

    init basic app

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import "./App.css"
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div>
+      <div class="container">
+        <div class="header">
+         <div class="img-area" >
+          <img src="assits\watsapp.jpg"></img>
+         </div>
+          <h2>Asif Ali</h2>
+          <h3>Front End Web-Developer</h3>
+        </div>
+
+        <div class="main">
+          <div class="left">
+            <h2><strong>Name</strong> Asif Ali</h2>
+            <p><strong>Age:</strong>26</p>
+            <p><strong>Email:</strong>asif7171786@gmail.com</p>
+
+            <p><strong>Phone:</strong>+92 3061367571</p>
+
+            <h2>Skills</h2>
+            <ul>
+              <li><strong>HTML/CSS</strong></li>
+              <li><strong>Javascript</strong></li>
+              <li><strong>React.js</strong> </li>
+            </ul>
+
+            <h2>Education</h2>
+            <h3>B.Sc in computer Science</h3>
+            <p>University of South Asia Lahore/Pakistan </p>
+
+            <h3>Intermediate</h3>
+            <p>Govt.Degree college Chichawatni </p>
+
+            <h3>Matriculation</h3>
+            <p>National Educational Complex Ali Town Ghaziabad </p>
+          </div>
+
+        <div class="right"><h2>Experience</h2>
+        <p> <strong>Three Month Internship as a front End developer</strong><br></br>Software House</p>    <br></br>
+        <p><strong>Administration in a school </strong><br></br>NEC School Ghaziabad </p>
+        
+        <div >
+        <h2>Certificates</h2>
+
+        <p><strong>Microsoft office course</strong><br></br>Vocational Trainig institute
+        </p>
+                <br></br>
+        <p><strong>Video Editing </strong><br></br>Online
+        </p>
+        </div>
+
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
+
+
